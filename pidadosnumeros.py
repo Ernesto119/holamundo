baa6b200cb21f3b1defce88e827204095a157dc7
Ernesto119 <153251265+Ernesto119@users.noreply.github.com>
2024-01-29 16:04:40 -0400

    Add files via upload

---
+++ pidadosnumeros.py
@@ -0,0 +1,7 @@
+n1 = float(input("Ingrese un numero: "))
+n2 = float(input("Ingrese otro numero: "))
+
+print("El cuadruple del producto es: ",(n1 * n2 * 4))
+print("El doble del cociente es: ",(n1/n2)*2)
+print("El cuadruple de su diferencia es: ", (n1-n2)*4)
+print("El cuadruple de su suma es:",(n1+n2)**2)
